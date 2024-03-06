6191cd123a2baa4cb9b5dfd0655ed497ab87a68b
om shankar <om8003golu@gmail.com>
2024-03-06 15:26:06 +0530

    memoization in js

--- JS/memoization.js
+++ JS/memoization.js
@@ -1,28 +1,57 @@
-function heavy(x) {
-  //   console.log("i am heavy fun " + x);
-  return "i am heavy fun " + x;
+// function heavy(x) {
+//   //   console.log("i am heavy fun " + x);
+//   return "i am heavy fun " + x;
+// }
+
+// // heavy(4);
+
+// function memoizationPattern(fx) {
+//   return function (x) {};
+// }
+
+// function memoizer(fx) {
+//   let map = new Map();
+//   return function (x) {
+//     if (map.has(x)) {
+//       return map.get(x);
+//     } else {
+//       let result = fx(x);
+//       map.set(x, result);
+//       return result;
+//     }
+//   };
+// }
+
+// let memoizedHeavy = memoizer(heavy);
+
+// console.log(memoizedHeavy(4));
+// console.log(memoizedHeavy(5));
+// -------------------------------------------------------------
+
+function sum(a) {
+  return a + 10;
 }
 
-// heavy(4);
-
-function memoizationPattern(fx) {
-  return function (x) {};
-}
-
-function memoizer(fx) {
+function memoization(fx) {
   let map = new Map();
-  return function (x) {
-    if (map.has(x)) {
-      return map.get(x);
-    } else {
-      let result = fx(x);
-      map.set(x, result);
+
+  return function (a) {
+    console.log(map);
+    if (map.has(a)) return map.get(a);
+    else {
+      let result = fx(a);
+      map.set(a, result);
+      console.log("else");
       return result;
     }
   };
 }
 
-let memoizedHeavy = memoizer(heavy);
-
-console.log(memoizedHeavy(4));
-console.log(memoizedHeavy(5));
+const memoizedSum = memoization(sum);
+console.log(memoizedSum(12));
+console.log(memoizedSum(13));
+console.log(memoizedSum(14));
+console.log(memoizedSum(14));
+console.log(memoizedSum(13));
+console.log(memoizedSum(14));
+console.log(memoizedSum(25));

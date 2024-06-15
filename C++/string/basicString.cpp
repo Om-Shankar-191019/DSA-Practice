b16b1402056731448168a6c6c1a8ada90d0ec237
om shankar <om8003golu@gmail.com>
2024-06-15 15:26:05 +0530

    frequency of characters in string

--- C++/string/basicString.cpp
+++ C++/string/basicString.cpp
@@ -10,11 +10,23 @@ void reverseString(string &str)
     }
 }
 
+void freqOfChars(string &s)
+{
+    unordered_map<char, int> mp;
+    for (int i = 0; i < s.length(); i++)
+    {
+        mp[s[i]]++;
+    }
+    for (auto x : mp)
+    {
+        cout << x.first << " : " << x.second << endl;
+    }
+}
 int main()
 {
 
-    string str = "omshankar";
-    reverseString(str);
+    string str = "geeksforgeeks";
+    freqOfChars(str);
     cout << str;
     return 0;
 }

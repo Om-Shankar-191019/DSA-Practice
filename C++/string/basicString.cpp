9c28f570ae2f5aeda166fe9987bffe1a8d737878
om shankar <om8003golu@gmail.com>
2024-06-15 15:33:57 +0530

    freq of charcters in alphabetical orders

--- C++/string/basicString.cpp
+++ C++/string/basicString.cpp
@@ -22,11 +22,33 @@ void freqOfChars(string &s)
         cout << x.first << " : " << x.second << endl;
     }
 }
+
+void freqOfChars2(string &s)
+{
+    int count[26] = {0};
+    for (int i = 0; i < s.length(); i++)
+    {
+        count[s[i] - 'a']++;
+    }
+
+    // freq of char print in same order.
+    // for (int i = 0; i < s.length(); i++)
+    // {
+    //     cout << s[i] << " : " << count[s[i] - 'a'] << endl;
+    // }
+
+    // freq of char print in alphabetical order.
+    for (int i = 0; i < 26; i++)
+    {
+        if (count[i] > 0)
+            cout << (char)(i + 97) << " : " << count[i] << endl;
+    }
+}
 int main()
 {
 
     string str = "geeksforgeeks";
-    freqOfChars(str);
+    freqOfChars2(str);
     cout << str;
     return 0;
 }

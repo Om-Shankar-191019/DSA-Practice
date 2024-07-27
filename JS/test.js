b65e668d6e15d9b34949232f1956922283e0aa47
om shankar <om8003golu@gmail.com>
2024-07-27 15:39:20 +0530

    call method in js and this keyword won't work in arrow fun

--- JS/test.js
+++ JS/test.js
@@ -1,11 +1,17 @@
-let object = {
-  k1: "value1",
-  k2: "value2",
-  k3: "value3",
-};
-let c = 0;
-for (let key in object) {
-  console.log(key + " ---> " + object[key]);
-  c++;
+var employee1 = { firstName: "John", lastName: "Rodson" };
+var employee2 = { firstName: "Jimmy", lastName: "Baily" };
+
+function invite(greeting1, greeting2) {
+  console.log(
+    greeting1 + " " + this.firstName + " " + this.lastName + ", " + greeting2
+  );
 }
-console.log(c);
+
+const inviteArrow = (greeting1, greeting2) => {
+  console.log(
+    greeting1 + " " + this.firstName + " " + this.lastName + ", " + greeting2
+  );
+};
+
+inviteArrow.call(employee1, "HELLO", "Stonish");
+invite.call(employee1, "HELLO", "Stonish");

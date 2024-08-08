da0285a84ec6113da1760e56168bce9963786a23
om shankar <om8003golu@gmail.com>
2024-08-08 12:23:01 +0530

    pattern 2 in js

--- JS/codingRound/hari.js
+++ JS/codingRound/hari.js
@@ -53,4 +53,17 @@ function pattern1(r) {
   }
 }
 
-pattern1(8);
+// **
+// ****
+// ******
+// ********
+function pattern2(n) {
+  for (let i = 1; i <= n; i++) {
+    let str = "";
+    for (let j = 1; j <= i; j++) {
+      str += "**";
+    }
+    console.log(str);
+  }
+}
+pattern2(4);

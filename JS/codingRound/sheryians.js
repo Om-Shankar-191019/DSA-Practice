0ab64e6d4bf1cea372750374ca6a01901e8f8409
om shankar <om8003golu@gmail.com>
2024-09-05 09:52:17 +0530

    reverse word from  given sentence 2

--- JS/codingRound/sheryians.js
+++ JS/codingRound/sheryians.js
@@ -1,15 +1,3 @@
-// function reverseString(str) {
-//   let i = 0;
-//   let j = str.length - 1;
-//   while (i < j) {
-//     let temp = str[i];
-//     str[i] = str[j];
-//     str[j] = temp;
-//     i++;
-//     j--;
-//   }
-//   return str;
-// }
 function reverseString(str) {
   let strArray = str.split("");
   let reverseStr = strArray.reverse().join("");
@@ -24,7 +12,17 @@ function reverseWordFromSent(sent) {
     result.push(reverseString(item));
   }
   let resultSent = result.join(" ");
-  console.log(resultSent);
+  return resultSent;
 }
 
-reverseWordFromSent("harse bhai kaise ho");
+// reverseWordFromSent("harse bhai kaise ho");
+
+function reverseWordFromSent2(sent) {
+  let arr = sent.split(" ");
+  let reversedArr = arr.map(function (item) {
+    return item.split("").reverse().join("");
+  });
+  return reversedArr.join(" ");
+}
+
+console.log(reverseWordFromSent2("harse bhai kaise ho"));
